22d0eb14b9136996381ae487d08be538a8474b28
katerinanosova <katerinanosova@gmail.com>
2024-01-31 04:04:00 +0000

    upd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import GlobalStyle from './GlobalStyles';
+import App from './src/App';
+import GlobalStyle from './src/GlobalStyles';
 
 
 
